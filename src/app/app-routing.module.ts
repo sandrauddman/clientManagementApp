1d2a47bc0da1c03f4b7f8a928489f1f834e47dc7
sandrauddman <sandra.uddman@gmail.com>
2025-09-25 14:56:47 +0200

    register page html created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientFormComponent } from './client-form/client-form.component';
 
 const routes: Routes = [
-  {path:"clienForm", component:ClientFormComponent}
+  {path:"clientForm", component:ClientFormComponent}
 ];
 
 @NgModule({
